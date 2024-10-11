7ee47b8c0a83a255a73e266348f989a71885a863
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-11 21:48:26 +0530

    Finished the UpdateViews backend API Functions

--- UpdateViews.js
+++ UpdateViews.js
@@ -1,3 +1,39 @@
-export default function UpdateViews() {
-    
+export default function UpdateViews(app, client) {
+    app.post('/updateViews/:id', async (req, res) => {
+        const { username } = req.body
+        const { id } = req.params
+
+        const database = client.db('prisma')
+        const mainstreamCollection = database.collection("mainstream")
+
+        try {
+            const viewedDoc = await mainstreamCollection.findOne({ id: parseInt(id) })
+
+            if (viewedDoc) {
+                await mainstreamCollection.updateOne(
+                    { id: parseInt(id) },
+                    { $addToSet: { viewedBy: username } }
+                )
+
+                const updatedDoc = await mainstreamCollection.findOne({ id: parseInt(id) })
+                const viewsCount = updatedDoc.viewedBy.length
+
+                await mainstreamCollection.updateOne(
+                    { id: parseInt(id) },
+                    { $set: { views: viewsCount } }
+                )
+
+                return res.status(200).json({
+                    message: "Data updated successfully",
+                    views: viewsCount
+                })
+            } else {
+                return res.status(404).json({ message: "Document not found in mainstream collection" })
+            }
+        } catch (error) {
+            console.error('Error updating collections:', error)
+            return res.status(500).json({ message: "Internal server error" })
+        }
+    })
+
 }
